c0ab20bde8cd989189b7b20a5ac8a47a2b600b6a
František Horinek <frantisekhorinek@gmail.com>
2019-01-30 16:26:41 +0100

    RAM shaving, mearging with PRs

---
+++ skydrop/utils/mem/analyse.py
@@ -0,0 +1,27 @@
+import os
+
+path = "../../Release/src"
+
+to_analyse = []
+
+def scan(path):
+    print(path)
+    for file in os.scandir(path):
+        
+        if file.is_dir():
+            scan(os.path.join(path, file.name))
+        
+        if file.name[-2:] == ".o":
+            to_analyse.append(os.path.join(path, file.name))
+        
+
+scan(path)        
+print(to_analyse)
+
+result = {}
+
+print ("   text       data        bss        dec        hex    filename")
+for file in to_analyse:
+    data = os.popen("avr-size %s" % file).read().split("\n")[1]
+    print(data)
+    
